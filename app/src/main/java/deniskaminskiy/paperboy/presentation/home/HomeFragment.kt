9a952354be2b97b7dc1e5ff31083e9a052b96f76
d.kaminskiy <kaminskiydis@gmail.com>
2019-06-06 18:41:06 +0300

    Some shits for coordinator

--- app/src/main/java/deniskaminskiy/paperboy/presentation/home/HomeFragment.kt
+++ app/src/main/java/deniskaminskiy/paperboy/presentation/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import deniskaminskiy.paperboy.R
 import deniskaminskiy.paperboy.core.BaseFragment
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : BaseFragment<HomePresenter, HomeView>(), HomeView {
 
@@ -23,7 +22,7 @@ class HomeFragment : BaseFragment<HomePresenter, HomeView>(), HomeView {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        vCollapsingToolbar.title = "Some Shit"
+
     }
 
 }
